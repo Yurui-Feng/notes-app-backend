46b48cb703e6ee9dfb46b5348acd5f62ca2f155a
Yurui-Feng <yurifeng@bu.edu>
2023-08-07 18:07:56 -0700

    add http cookie configs

--- app.js
+++ app.js
@@ -65,6 +65,12 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+    cookie: {
+      sameSite: "none",
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7, // One week
+      httpOnly: true,
+    },
   })
 );
 // setup passport for authentication
